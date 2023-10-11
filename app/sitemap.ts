f3aad959ff34916b3eab15ca309bb721b6156a6e
Quddarat Mushtaq <quddaratmushtaq@startwithyou.ventures>
2023-10-11 03:31:36 -0400

    Updated sitemap and demo page.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,11 +3,11 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://demo.useliftoff.com",
+      url: "https://demo.startwithyou.ventures",
       lastModified: new Date(),
     },
     {
-      url: "https://demo.useliftoff.com/demo",
+      url: "https://demo.startwithyou.ventures/demo",
       lastModified: new Date(),
     },
   ];
